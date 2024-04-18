01e120d8fdbe13146138d6a08a0f2f34918bc5cb
Lee Griever <leeg@moveo.co.il>
2024-04-18 11:25:25 +0300

    header pr notes fixed

--- src/components/SearchBar/SearchBar.tsx
+++ src/components/SearchBar/SearchBar.tsx
@@ -4,6 +4,9 @@ import './SearchBar.scss'
 function SearchBar() {
   return (
     <div className="search-bar-container">
+    <h2 className='search-title'>
+      
+    </h2>
     </div>
   )
 }
--- src/data/constants.ts
+++ src/data/constants.ts
@@ -4,3 +4,16 @@ export const paths = {
     restaurants: "/restaurants",
     internalPage: "/restaurant/:restaurantId"
 };
+
+export const text = {
+    searchBarTitle: "Epicure works with the top chef restaurants in Tel Aviv",
+    searchBarInput: "Search for restaurant cuisine, chef",
+};
+
+// export const breakPoints = {
+//     xs: 300,
+//     s: 500,
+//     m: 768, 
+//     l: 1000,
+//     xl: 1440,
+// };
